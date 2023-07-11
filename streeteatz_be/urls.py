fb9da08e7187bcc66210c9cc39d6ce1a2c2d31fb
Reilly Miller <reillyjmiller31@gmail.com>
2023-07-10 20:49:42 -0600

    add vendors path for api

--- streeteatz_be/urls.py
+++ streeteatz_be/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from streeteatz.views import vendor_list
+# from Vendor import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('vendors/', vendor_list),
+    ]
