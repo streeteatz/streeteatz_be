377bf4d6caae02a3cbbc2110d88cdadbc55d8adf
Isaac Thill <ithill22@turing.edu>
2023-07-12 17:16:32 -0600

    add items

--- streeteatz/admin.py
+++ streeteatz/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Vendor
+from .models import Item
 
 # Register your models here.
 admin.site.register(Vendor)
+admin.site.register(Item)
--- streeteatz/serializers.py
+++ streeteatz/serializers.py
@@ -1,7 +1,13 @@
 from .models import Vendor
+from .models import Item
 from rest_framework import serializers
 
 class VendorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vendor
         fields = ['id', 'name', 'phone_number', 'location', 'address', 'status', 'hours', 'description', 'tags', 'website', 'img', 'wait_time', 'upvote', 'downvote', 'favorited', 'up_rating', 'down_rating', 'distanceFromUser']
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ['id', 'name', 'price', 'description', 'vendor']
