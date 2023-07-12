8473b4a555e01838a921408a279922119e46ce1c
Reilly Miller <reillyjmiller31@gmail.com>
2023-07-12 15:37:15 -0600

    update settings

--- streeteatz_be/settings.py
+++ streeteatz_be/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'streeteatz_be.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/streeteatz',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'streeteatz',
+        'USER': 'new_streeteatz',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
 
 
